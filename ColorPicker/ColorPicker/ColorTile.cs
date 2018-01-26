ac76f7ac37e4eaa67c31679e947861246de79d1d
llNovall <nels.puthoor@hotmail.com>
2018-01-26 16:41:45 +0000

    Initial Commit

---
+++ ColorPicker/ColorPicker/ColorTile.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColorPicker
+{
+    class ColorTile
+    {
+        private string colorName;
+
+
+        #region Accessors
+        public string ColorName
+        {
+            get
+            {
+                return colorName;
+            }
+
+            set
+            {
+                colorName = value;
+            }
+        }
+
+        #endregion
+        public ColorTile(string n)
+        {
+            ColorName = n;
+        }
+
+
+    }
+}
